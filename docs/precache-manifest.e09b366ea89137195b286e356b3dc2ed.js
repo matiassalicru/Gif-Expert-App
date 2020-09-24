54275aa84f660af082319b58180b4632062dcec2
Matias Salicru <mattiassalicru@gmail.com>
2020-09-24 08:51:17 -0300

    index acutalizado

---
+++ docs/precache-manifest.e09b366ea89137195b286e356b3dc2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3370aa34e35afd4743cb23a74e1bcd3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adaa41caeff75e60626c",
+    "url": "/static/css/main.9f4b89f3.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaa41caeff75e60626c",
+    "url": "/static/js/main.bade684b.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
