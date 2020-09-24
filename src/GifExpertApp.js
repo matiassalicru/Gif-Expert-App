00001bafb928e9b0fdd79cd5cfadfd1556dfcbf7
Matias Salicru <mattiassalicru@gmail.com>
2020-09-24 08:37:48 -0300

    New intro

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
-export const GifExpertApp = ({ defaultCategories= []}) => {
+export const GifExpertApp = ({ defaultCategories= ['The office']}) => {
   const [categories, setCategories] = useState(defaultCategories);
 
   return (
